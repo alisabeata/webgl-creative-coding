fad2a5647222a601f65d96b92f27c61267b999c1
Alisa Vasilenko <alisa314159@gmail.com>
2024-04-23 17:27:03 +0200

    upd template

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import ThreeCanvas from './components/ThreeCanvas'
 // https://threejs.org/
 // https://openframeworks.cc/ openFrameworks is an open source toolkit designed for creative coding founded by Zachary Lieberman
 // https://github.com/mattdesl/workshop-generative-art
+// demos: https://three-demos.glitch.me/
 
 const App = () => <ThreeCanvas />
 
